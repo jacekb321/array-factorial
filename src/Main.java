043208f8ac0873c208d7aa47321b1d7892b608f5
jacekb321 <jacekb321@gmail.com>
2026-02-06 13:29:09 +0100

    Final project

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("how big array u want");
+        System.out.println("how big array u want(at least 1 character inside;))");
         int arraySize = getInt();
         int[] array = new int[arraySize];
         System.out.println("Give me " +arraySize+" numbers");
-        System.out.println("First number ");
 
         boolean isNextNumber = true;
 
-        do{
 
-        }while (isNextNumber);
+        for(int i=0;i<arraySize;i++){
+            int number = getInt();
+            array[i] = number;
+        }
 
 
+        System.out.println(Arrays.toString(array));
+        int [] newArray = new int[arraySize];
+        int placeInArray = 0;
 
-        System.out.println("Gimme number");
-        int a = getInt();
-        int factorial = factorialNumber(a);
-        System.out.println(factorial);
+        for (int a : array){
+            int factorial = factorialNumber(a);
+            newArray[placeInArray]=factorial;
+            placeInArray++;
+        }
+        System.out.println("Array with factorial of numbers u gave");
+        System.out.println(Arrays.toString(newArray));
+
+
         }
 
 
